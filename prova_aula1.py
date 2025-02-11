723f956114cc080bb61a61a3d613a218089cc646
CunhaLucasDev <cunhalucasdev@outlook.com>
2025-02-11 20:31:15 -0300

    Update prova_aula1.py

--- prova_aula1.py
+++ prova_aula1.py
@@ -1,5 +1,5 @@
 # Programa para calculo de área de um retângulo sendo a mesma calculada pela formula : base vezes altura.
 Base = int(input("Informe a base do retângulo:"))
-Altura = int(input("informe a altura do retângulo:"))
+Altura =  int(input("informe a altura do retângulo:"))
 Area = (Base)*(Altura)
 print(Area)
